6d46e3c2efef3073cb581c7c7cae7343dffe82ab
Juan Carlos Tong <juancarlos.tong@optimizely.com>
2019-08-28 17:13:10 -0700

    dont make ls error

--- scripts/devops_create_packages.sh
+++ scripts/devops_create_packages.sh
@@ -5,6 +5,7 @@ if [[ $TRAVIS_OS_NAME == "linux" ]]; then
   cd $TRAVIS_BUILD_DIR
   make devops_build_fpm_centos
   make devops_get_fpm_centos
+  ls -al *.rpm
 elif [[ $TRAVIS_OS_NAME == "osx" ]]; then
   echo "we're on osx"
 else
