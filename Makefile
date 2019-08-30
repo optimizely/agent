85aa78c80e544e3151871a503805474c3c6e5ec3
Juan Carlos Tong <juancarlos.tong@optimizely.com>
2019-08-30 13:33:15 -0700

    change devops to ci

--- Makefile
+++ Makefile
@@ -46,5 +46,7 @@ lint:
 run: $(TARGET) ## run
 	$(GOBIN)/$(TARGET)
 
+include scripts/Makefile.ci
+
 help: ## help
 	@awk 'BEGIN {FS = ":.*?## "} /^[a-zA-Z_-]+:.*?## / {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
--- scripts/ci_creates_packages.sh
+++ scripts/ci_creates_packages.sh
@@ -3,11 +3,11 @@
 if [[ $TRAVIS_OS_NAME == "linux" ]]; then
   echo "we're on linux"
   cd $TRAVIS_BUILD_DIR
-  make devops_build_fpm_centos
-  make devops_get_fpm_centos
+  make ci_build_fpm_centos
+  make ci_get_fpm_centos
   ls -al *.rpm
-  make devops_build_fpm_ubuntu
-  make devops_get_fpm_ubuntu
+  make ci_build_fpm_ubuntu
+  make ci_get_fpm_ubuntu
   ls -al *.deb
 elif [[ $TRAVIS_OS_NAME == "osx" ]]; then
   echo "we're on osx"
