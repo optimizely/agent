b4da3246408d002048d22348e2b1162b59e4ba1c
Matt Carroll <matt.carroll@optimizely.com>
2019-10-11 20:51:10 -0700

    Update nsq_queue_test

--- pkg/event/nsq_queue_test.go
+++ pkg/event/nsq_queue_test.go
@@ -24,51 +24,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// right now we create a embedded nsqd and send events to it.  this fully tests nsqd but
-// is probably not necessary for our tests.  in order to stub the processor and consumer out,
-// we need to abstract away the producer and consumer interface, then implement one for nsq and one
-// that stubs nsq.
 func TestNSQQueue_Add_Get_Size_Remove(t *testing.T) {
-	q := NewNSQueueDefault()
-
-	impression := BuildTestImpressionEvent()
-	conversion := BuildTestConversionEvent()
-
-	q.Add(impression)
-	q.Add(impression)
-	q.Add(conversion)
-
-	time.Sleep(1 * time.Second)
-
-	items1 := q.Get(2)
-
-	assert.NotNil(t, items1)
-
-	q.Remove(1)
-
-	time.Sleep(1 * time.Second)
-
-	items2 := q.Get(1)
-
-	assert.True(t, len(items2) >= 0)
-
-	time.Sleep(1 * time.Second)
-
-	allItems := q.Remove(3)
-
-	assert.True(t, len(allItems) >= 0)
-
-	assert.Equal(t, 0, q.Size())
-}
-
-func TestNSQQueue_TestConfigNoProducerConsumer(t *testing.T) {
 	pc := make(chan snsq.ProducerRequest, 10)
 	cc := make(chan snsq.Message, 10)
+	defer func() {
+		close(cc)
+		close(pc)
+	}()
+
 	go func() {
 		for pr := range pc {
 			pr.Response <- nil
+			cc <- *snsq.NewMessage(0, pr.Message, make(chan snsq.Command, 10))
 		}
 	}()
+
 	q := NewNSQueue(10, "", false, pc, cc)
 
 	impression := BuildTestImpressionEvent()
@@ -78,19 +48,14 @@ func TestNSQQueue_TestConfigNoProducerConsumer(t *testing.T) {
 	q.Add(impression)
 	q.Add(conversion)
 
-	items1 := q.Get(2)
-
-	assert.Equal(t, 0, len(items1))
+	items1 := q.Get(3)
+	assert.Eventually(t, func() bool { return len(items1) == 3 }, 5*time.Second, 10*time.Millisecond)
 
 	q.Remove(1)
-
-	items2 := q.Get(1)
-
-	assert.Equal(t, 0, len(items2))
+	items2 := q.Get(3)
+	assert.Equal(t, 2, len(items2))
 
 	allItems := q.Remove(3)
-
-	assert.Equal(t, 0, len(allItems))
-
+	assert.Equal(t, 2, len(allItems))
 	assert.Equal(t, 0, q.Size())
 }
