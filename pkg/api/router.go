801325ee6d3e90be243cf76295adce77a12c3e74
Matt Carroll <matt.carroll@optimizely.com>
2019-11-12 16:10:52 -0800

    Remove unnecessary space in router

--- pkg/api/router.go
+++ pkg/api/router.go
@@ -69,7 +69,6 @@ func NewRouter(opt *RouterOptions) *chi.Mux {
 
 		r.With(middleware.Metricize("get-user-feature")).Get("/features/{featureKey}", opt.userAPI.GetFeature)
 		r.With(middleware.Metricize("track-user-feature")).Post("/features/{featureKey}", opt.userAPI.TrackFeature)
-
 		r.With(middleware.Metricize("get-variation")).Get("/experiments/{experimentKey}", opt.userAPI.GetVariation)
 		r.With(middleware.Metricize("set-forced-variation")).Put("/experiments/{experimentKey}/variations/{variationKey}", opt.userAPI.SetForcedVariation)
 		r.With(middleware.Metricize("delete-forced-variation")).Delete("/experiments/{experimentKey}/variations", opt.userAPI.RemoveForcedVariation)
