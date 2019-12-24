3843dd09f3e65abde0bb8a5dc68b377db81a8e73
Juan Carlos Tong <juancarlos.tong@optimizely.com>
2019-12-24 05:01:37 -0800

    add noninteractive mode to build.ps1

--- build.ps1
+++ build.ps1
@@ -1,14 +1,18 @@
 #Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
 
-function checkGo {
-    $software = "Go Programming Language amd64 go1.13.5";
+function checkPrereq($software, $URL, $SHA, $mode) {
+
     $installed = (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where { $_.DisplayName -eq $software }) -ne $null
 
     If(-Not $installed) {
-	    Write-Host "'$software' NOT is installed.";
-        $answer = Read-Host -Prompt "Install? (y/n)"
+	    Write-Host "'$software' is NOT installed.";
+        if ($mode -eq "noninteractive") {
+            $answer = "y"
+        } else {
+            $answer = Read-Host -Prompt "Install? (y/n)"
+        }
         if ($answer -eq "y") {
-            installGo
+            installPrereq $URL $SHA
         }else{
             Write-Host "Aborted by user."
             exit 0
@@ -18,28 +22,47 @@ function checkGo {
     }
 }
 
-function installGo {
-    $URL = "https://dl.google.com/go/go1.13.5.windows-amd64.msi"
+function installPrereq($URL, $SHA) {
+    $RANDOM = Get-Random
     $filename = $URL.Substring($URL.LastIndexOf("/") + 1)
-    (New-Object System.Net.WebClient).DownloadFile("https://dl.google.com/go/go1.13.5.windows-amd64.msi","$PSScriptRoot\$filename")
-
+    # https://github.com/lukesampson/scoop/pull/2065#issuecomment-369669048
+    if (([System.Net.ServicePointManager]::SecurityProtocol).HasFlag([Net.SecurityProtocolType]::Tls12) -eq $false) {
+        [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+    }
+    (New-Object System.Net.WebClient).DownloadFile("$URL","$PSScriptRoot\$filename")
+    Write-Host "$PSScriptRoot\$filename" downloaded. -ForegroundColor Green
     $hashFromFile = get-filehash -Path "$PSScriptRoot\$filename" -Algorithm SHA256
-    # checksum from https://golang.org/dl/
-    $verifychecksum = "eabcf66d6d8f44de21a96a18d957990c62f21d7fadcb308a25c6b58c79ac2b96"
 
-    if ($hashFromFile.Hash -eq $verifychecksum.toUpper()) {
-        Write-Host "$PSScriptRoot\$filename" verified -ForegroundColor Green
-        Start-Process -FilePath "msiexec.exe" -ArgumentList "/i","$PSScriptRoot\$filename","INSTALLDIR=$env:APPDATA\go","/qb" -Wait
+    if ($hashFromFile.Hash -eq $SHA.toUpper()) {
+        Write-Host "$PSScriptRoot\$filename" verified. -ForegroundColor Green
+        $extension = $filename.Substring($filename.LastIndexOf("."))
+        if ($extension -eq ".msi") {
+            Start-Process -FilePath "msiexec.exe" -ArgumentList "/i","$PSScriptRoot\$filename","INSTALLDIR=$env:APPDATA\$RANDOM","/qb" -Wait
+        } elseif ($extension -eq ".exe") {
+            Start-Process -FilePath "$PSScriptRoot\$filename" -ArgumentList "/DIR=$env:APPDATA\$RANDOM","/VERYSILENT"
+        } else {
+            Write-Host "Unrecognized extension: $extension" -ForegroundColor Red
+            exit 1
+        }
+        Write-Host "$PSScriptRoot\$filename" installed. -ForegroundColor Green
     } else {
-        Write-Host "$PSScriptRoot\$filename" is corrupt -ForegroundColor Red
+        Write-Host "$PSScriptRoot\$filename" is corrupt. -ForegroundColor Red
+        Write-Host "expected: "$SHA.toUpper()
+        Write-Host "got:       "$hashFromFile.Hash
         exit 1
     }
 }
 
 function buildOptimizelyAgent {
     Write-Host "Building Optimizely Agent..." -ForegroundColor Green
+    git status
+    if ( $LASTEXITCODE -ne 0 ) {
+        Write-Host "Optimizely Agent needs to build from its own git repository in order to determine its version." -ForegroundColor Red
+        exit 1
+    }
     $env:GO111MODULE = "on"
-    go build -ldflags "-s -w -X main.Version=0.8.1" -o bin\optimizely.exe cmd\sidedoor\main.go
+    $VERSION = (git describe --tags)
+    go build -ldflags "-s -w -X main.Version=$VERSION" -o bin\optimizely.exe cmd\sidedoor\main.go
     dir bin
 }
 
@@ -60,20 +83,30 @@ function runOptimizelyAgentTests {
     }
 }
 
-function main {
+function main($mode) {
 
-# check if go is installed, if not, install it.
-checkGo
+    # noninteractive mode: ./build.ps1 noninteractive (default: interactive)
 
-# refresh $PATH
-refreshPath
 
-# run tests
-runOptimizelyAgentTests
+    # check if go is installed, if not, install it.
+    checkPrereq 'Go Programming Language amd64 go1.13.5' https://dl.google.com/go/go1.13.5.windows-amd64.msi eabcf66d6d8f44de21a96a18d957990c62f21d7fadcb308a25c6b58c79ac2b96 $mode
+    # same but with git
+    checkPrereq 'Git version 2.24.1.2' https://github.com/git-for-windows/git/releases/download/v2.24.1.windows.2/Git-2.24.1.2-64-bit.exe 34e484936105713e7d0c2f421bf62e4cfe652f6638a9ecb5df2186c1918753e2 $mode
 
-# build optimizely agent
-buildOptimizelyAgent
+    # refresh $PATH
+    refreshPath
+
+    # run tests
+    runOptimizelyAgentTests
+
+    # build optimizely agent
+    buildOptimizelyAgent
 
 }
 
-main
+
+$mode = $args[0]
+if ($mode -ne "noninteractive") {
+    $mode = "interactive"
+}
+main $mode
