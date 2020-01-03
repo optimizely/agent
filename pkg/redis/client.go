38bc0c3c4c5f92ce104cf71f55b0fb2660266eac
Mike Davis <mike@optimizely.com>
2020-01-03 08:23:57 -0800

    WIP

---
+++ pkg/redis/client.go
@@ -0,0 +1,20 @@
+package redis
+
+import (
+	"fmt"
+	"github.com/go-redis/redis/v7"
+)
+
+func NewClient() *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
+	// Output: PONG <nil>
+
+	return client
+}
