3797ffbd8c815de73110b4f7634a539f245e87aa
Juan Carlos Tong <juancarlos.tong@optimizely.com>
2020-01-09 15:13:28 -0800

    $pid is reserved in powershell

--- scripts/check_exe.ps1
+++ scripts/check_exe.ps1
@@ -1,5 +1,5 @@
 Write-Host "Checking if bin\optimizely.exe  runs..." -ForegroundColor Green
-$pid = Start-Process -Filepath "bin\optimizely.exe" -NoNewWindow -PassThru
+$OPTLY_PID = Start-Process -Filepath "bin\optimizely.exe" -NoNewWindow -PassThru
 Start-Sleep -s 5
 Write-Host "Stopping bin\optimizely.exe..." -ForegroundColor Green
-Stop-Process $pid
+Stop-Process $OPTLY_PID
