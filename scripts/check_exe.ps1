cd0915edacba3f57baf218545da8619ca598e583
Juan Carlos Tong <juancarlos.tong@optimizely.com>
2020-01-09 19:34:34 -0800

    verify that optimizely.exe actually works

--- scripts/check_exe.ps1
+++ scripts/check_exe.ps1
@@ -1,5 +1,13 @@
-Write-Host "Checking if bin\optimizely.exe  runs..." -ForegroundColor Green
+Write-Host "Checking if bin\optimizely.exe works..." -ForegroundColor Green
+
 $OPTLY_PID = Start-Process -Filepath "bin\optimizely.exe" -NoNewWindow -PassThru
 Start-Sleep -s 5
+
+if( Invoke-RestMethod -uri 127.0.0.1:8088/health -Method Get | Where-Object { $_.status -match "ok" } ){
+  Write-Host "Success: /health endpoint is working"
+}else{
+  throw "Failure: /health endpoint is not working"
+}
+
 Write-Host "Stopping bin\optimizely.exe..." -ForegroundColor Green
 Stop-Process $OPTLY_PID
