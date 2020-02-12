dddba25dc27c1344918a112fbbb6e8a59d45ab64
Mike Davis <mike@optimizely.com>
2020-02-12 09:37:31 -0800

    Add config handler. (#142)

---
+++ pkg/handlers/optimizely_config_test.go
@@ -0,0 +1,75 @@
+package handlers
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/go-chi/chi"
+	"github.com/optimizely/go-sdk/pkg/config"
+	"github.com/stretchr/testify/suite"
+
+	"github.com/optimizely/agent/pkg/middleware"
+	"github.com/optimizely/agent/pkg/optimizely"
+	"github.com/optimizely/agent/pkg/optimizely/optimizelytest"
+)
+
+type OptimizelyConfigTestSuite struct {
+	suite.Suite
+	oc  *optimizely.OptlyClient
+	tc  *optimizelytest.TestClient
+	mux *chi.Mux
+}
+
+func (suite *OptimizelyConfigTestSuite) ClientCtx(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := context.WithValue(r.Context(), middleware.OptlyClientKey, suite.oc)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+// Setup Mux
+func (suite *OptimizelyConfigTestSuite) SetupTest() {
+	testClient := optimizelytest.NewClient()
+	optlyClient := &optimizely.OptlyClient{
+		OptimizelyClient: testClient.OptimizelyClient,
+		ConfigManager:    nil,
+		ForcedVariations: testClient.ForcedVariations,
+	}
+
+	mux := chi.NewMux()
+	mux.With(suite.ClientCtx).Get("/config", OptimizelyConfig)
+
+	suite.oc = optlyClient
+	suite.tc = testClient
+	suite.mux = mux
+}
+
+func (suite *OptimizelyConfigTestSuite) TestConfig() {
+	req := httptest.NewRequest("GET", "/config", nil)
+	rec := httptest.NewRecorder()
+	suite.mux.ServeHTTP(rec, req)
+	suite.Equal(http.StatusOK, rec.Code)
+
+	// Unmarshal response
+	var actual config.OptimizelyConfig
+	err := json.Unmarshal(rec.Body.Bytes(), &actual)
+	suite.NoError(err)
+
+	suite.Equal(*suite.oc.GetOptimizelyConfig(), actual)
+}
+
+// In order for 'go test' to run this suite, we need to create
+// a normal test function and pass our suite to suite.Run
+func TestOptimizelyConfigTestSuite(t *testing.T) {
+	suite.Run(t, new(OptimizelyConfigTestSuite))
+}
+
+func TestOptimizelyConfigMissingOptlyCtx(t *testing.T) {
+	req := httptest.NewRequest("POST", "/", nil)
+	rec := httptest.NewRecorder()
+	http.HandlerFunc(OptimizelyConfig).ServeHTTP(rec, req)
+	assertError(t, rec, "optlyClient not available", http.StatusInternalServerError)
+}
