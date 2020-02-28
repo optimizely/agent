e1d7f14e92bca9cdfbfca204b005913d88854aaf
Matjaz Pirnovar <matjaz.pirnovar@optimizely.com>
2020-02-27 18:25:05 -0800

    simplified run_agent.sh

--- tests/acceptance/run_agent.sh
+++ tests/acceptance/run_agent.sh
@@ -2,11 +2,15 @@
 
 # This conditional is to be able to run the Agent when tests are in "agent" repo as well as
 # when tests are in "acceptance" repo. Because paths are different.
+#echo $PWD
+#if [[ $PWD == */agent/tests/acceptance ]]
+#then
+#    cd ../../
+#fi
+#
+#make run
+
+
 echo $PWD
-if [[ $PWD == */agent/tests/acceptance ]]
-then
-    cd ../../
-fi
-
+cd ../../
 make run
-
