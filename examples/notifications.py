12ec78d8415ce0c4c6a7c4821346451f554bd440
Tom Zurkan <thomas.zurkan@optimizely.com>
2020-03-19 13:52:14 -0700

    fix exception type

--- examples/notifications.py
+++ examples/notifications.py
@@ -1,18 +1,17 @@
 from sseclient import SSEClient
-from httplib import IncompleteRead
+from six.moves.http_client import IncompleteRead
 
 def get_messages():
     requests_kwargs = {'headers': {}}
     requests_kwargs['headers']['X-Optimizely-SDK-Key'] = 'FeEqVPw2ZKLcxjHX5A732L'
     print('here')
-    messages = SSEClient('http://localhost:8080/v1/notifications/event-stream', timeout=10000, chunk_size=128,
+    messages = SSEClient('http://localhost:8080/v1/notifications/event-stream', timeout=100000, chunk_size=128,
                          headers={'X-Optimizely-SDK-Key': 'FeEqVPw2ZKLcxjHX5A732L'})
     try:
         for msg in messages:
             print(msg)
     except IncompleteRead as e:
         print(e.partial)
-
 
 while True:
     try:
