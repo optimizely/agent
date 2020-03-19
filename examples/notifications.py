8b90d9886b8bd27efdebea8a156ba151de6ced30
Tom Zurkan <thomas.zurkan@optimizely.com>
2020-03-19 13:59:32 -0700

    fix pr mistakes

--- examples/notifications.py
+++ examples/notifications.py
@@ -1,9 +1,11 @@
+#!/usr/bin/python
+# example: python notifications.py <SDK-Key>
+# This example shows how to attach and consume notifications from local running instance of the agent.
+
 from sseclient import SSEClient
 from six.moves.http_client import IncompleteRead
 
 def get_messages():
-    requests_kwargs = {'headers': {}}
-    requests_kwargs['headers']['X-Optimizely-SDK-Key'] = 'FeEqVPw2ZKLcxjHX5A732L'
     print('here')
     messages = SSEClient('http://localhost:8080/v1/notifications/event-stream', timeout=100000, chunk_size=128,
                          headers={'X-Optimizely-SDK-Key': 'FeEqVPw2ZKLcxjHX5A732L'})
