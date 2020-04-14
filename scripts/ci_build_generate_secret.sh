bbd8cc785007966a44b92016edc8c358047fe7ef
Juan Carlos Tong <juancarlos.tong@optimizely.com>
2020-04-13 19:57:50 -0700

    fix parameter expansion

--- scripts/ci_build_generate_secret.sh
+++ scripts/ci_build_generate_secret.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 cd $TRAVIS_BUILD_DIR
-docker run -v $PWD:/workdir -v /tmp/output_packages:/output -it golang:${GIMME_GO_VERSION:.x=} bash -c "cd /workdir && make build_generate_secret && cp /workdir/bin/generate_secret /output"
+docker run -v $PWD:/workdir -v /tmp/output_packages:/output -it golang:${GIMME_GO_VERSION%.x} bash -c "cd /workdir && make build_generate_secret && cp /workdir/bin/generate_secret /output"
