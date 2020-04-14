82ca409e55f530bdad7875ccd7e614a486bf57f9
Juan Carlos Tong <juancarlos.tong@optimizely.com>
2020-04-14 00:24:21 -0700

    add md5sum and run it

--- scripts/ci_build_generate_secret.sh
+++ scripts/ci_build_generate_secret.sh
@@ -3,3 +3,7 @@ set -euo pipefail
 
 cd $TRAVIS_BUILD_DIR
 docker run -v $PWD:/workdir -v /tmp/output_packages:/output -it golang:${GIMME_GO_VERSION%.x} bash -c "cd /workdir && make build_generate_secret && cp /workdir/bin/generate_secret /output"
+
+# try running generate_secret
+/tmp/output_packages/generate_secret
+md5sum /tmp/output_packages/generate_secret
