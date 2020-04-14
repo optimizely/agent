650c9eb4e6b3a5aa41a7d2dc398e003f86735ef7
Juan Carlos Tong <juancarlos.tong@optimizely.com>
2020-04-13 19:29:44 -0700

    ci(travis): build generate_secret tool in ci

---
+++ scripts/ci_build_generate_secret.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cd $TRAVIS_BUILD_DIR
+docker run -v $PWD:/workdir -v /tmp/output_packages:/output -it golang:${GIMME_GO_VERSION:.x=} bash -c "cd /workdir && make build_generate_secret && cp /workdir/bin/generate_secret /output"
