d84d676e265b89c89bd613694789587fcb409871
JC <40373238+juancarlostong@users.noreply.github.com>
2020-04-21 12:50:25 -0700

    ci(travis): build generate_secret for windows and macos (#217)
    * build generate_secret for windows and macos
    
    * create tar.gz archive
    
    * add windows build and tests for all releases

--- scripts/ci_build_generate_secret.sh
+++ scripts/ci_build_generate_secret.sh
@@ -2,8 +2,19 @@
 set -euo pipefail
 
 cd $TRAVIS_BUILD_DIR
-docker run -v $PWD:/workdir -v /tmp/output_packages:/output -it golang:${GIMME_GO_VERSION%.x} bash -c "cd /workdir && make build_generate_secret && cp /workdir/bin/generate_secret /output"
 
-# try running generate_secret
-/tmp/output_packages/generate_secret
-md5sum /tmp/output_packages/generate_secret
+for os in linux darwin windows; do
+  [[ $os == 'windows' ]] && windows=1
+  docker run \
+    -e GOOS=$os \
+    -e GOARCH=amd64 \
+    -v $PWD:/workdir \
+    -v /tmp/output_packages:/output \
+    -it golang:${GIMME_GO_VERSION%.x} \
+    bash -c "cd /workdir && make clean && make build_generate_secret \
+    && cp /workdir/bin/generate_secret /output/generate_secret${windows:+.exe} \
+    && tar cvfz /output/generate_secret-$os-amd64-$APP_VERSION.tar.gz -C /output generate_secret${windows:+.exe} \
+    && rm /output/generate_secret${windows:+.exe}"
+  ls -al /tmp/output_packages/generate_secret-$os-amd64-$APP_VERSION.tar.gz
+  md5sum /tmp/output_packages/generate_secret-$os-amd64-$APP_VERSION.tar.gz
+done
