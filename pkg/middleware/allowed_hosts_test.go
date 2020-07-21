8f9f40bf3b57f9851076648e8a41d7ec1508c463
Matt Carroll <matt.carroll@optimizely.com>
2020-07-21 10:23:11 -0700

    Reduce boilerplate in tests

--- pkg/middleware/allowed_hosts_test.go
+++ pkg/middleware/allowed_hosts_test.go
@@ -38,64 +38,52 @@ func (s *AllowedHostsTestSuite) SetupTest() {
 	s.handler = AllowedHosts([]string{"76.125.27.44", "example.com"}, "8080", true)(okHandler)
 }
 
-func (s *AllowedHostsTestSuite) TestRequestHostMatchesFirstAllowed() {
-	req := httptest.NewRequest("GET", "https://76.125.27.44:8080/v1/config", nil)
-	rec := httptest.NewRecorder()
-	s.handler.ServeHTTP(rec, req)
-	s.Equal(http.StatusOK, rec.Code)
+func (s *AllowedHostsTestSuite) TestURLHostAndPort() {
+	scenarios := []struct {
+		inputUrl       string
+		expectedStatus int
+	}{
+		// matches first allowedHost, expect StatusOK
+		{"https://76.125.27.44:8080/v1/config", http.StatusOK},
+		// matches second allowedHost, expect StatusOK
+		{"https://example.com:8080/v1/config", http.StatusOK},
+		// wrong URL port, expect http.StatusNotFound
+		{"https://76.125.27.44:1000/v1/config", http.StatusNotFound},
+		// wrong URL host, expect http.StatusNotFound
+		{ "https://evil.com:8080/v1/config", http.StatusNotFound},
+	}
+
+	for _, scenario := range scenarios {
+		req := httptest.NewRequest("GET", scenario.inputUrl, nil)
+		rec := httptest.NewRecorder()
+		s.handler.ServeHTTP(rec, req)
+		s.Equal(scenario.expectedStatus, rec.Code)
+	}
 }
 
-func (s *AllowedHostsTestSuite) TestRequestHostMatchesSecondAllowed() {
-	req := httptest.NewRequest("GET", "https://example.com:8080/v1/config", nil)
-	rec := httptest.NewRecorder()
-	s.handler.ServeHTTP(rec, req)
-	s.Equal(http.StatusOK, rec.Code)
-}
+func (s *AllowedHostsTestSuite) TestCustomHeaders() {
+	scenarios := []struct {
+		inputHeaderKey string
+		inputHeaderVal string
+		expectedStatus int
+	} {
+		// X-Forwarded-Host is valid, expect http.statusOK
+		{"X-Forwarded-Host", "example.com:8080", http.StatusOK},
+		// X-Forwarded-Host is invalid, expect http.statusNotFound
+		{"X-Forwarded-Host", "evil.com:8080", http.StatusNotFound},
+		// Forwarded is valid, expect http.statusOK
+		{"Forwarded", "host=76.125.27.44:8080", http.StatusOK},
+		// Forwarded is invalid, expect http.statusOK
+		{"Forwarded", "host=77.125.26.44:8080", http.StatusNotFound},
+	}
 
-func (s *AllowedHostsTestSuite) TestRequestHostWrongPort() {
-	req := httptest.NewRequest("GET", "https://76.125.27.44:1000/v1/config", nil)
-	rec := httptest.NewRecorder()
-	s.handler.ServeHTTP(rec, req)
-	s.Equal(http.StatusNotFound, rec.Code)
-}
-
-func (s *AllowedHostsTestSuite) TestRequestHostWrongAddr() {
-	req := httptest.NewRequest("GET", "https://evil.com:8080/v1/config", nil)
-	rec := httptest.NewRecorder()
-	s.handler.ServeHTTP(rec, req)
-	s.Equal(http.StatusNotFound, rec.Code)
-}
-
-func (s *AllowedHostsTestSuite) TestXForwardedHostValid() {
-	req := httptest.NewRequest("GET", "https://company-proxy.com:8080/v1/config", nil)
-	req.Header.Set("X-Forwarded-Host", "example.com:8080")
-	rec := httptest.NewRecorder()
-	s.handler.ServeHTTP(rec, req)
-	s.Equal(http.StatusOK, rec.Code)
-}
-
-func (s *AllowedHostsTestSuite) TestXForwardedHostInvalid() {
-	req := httptest.NewRequest("GET", "https://company-proxy.com:8080/v1/config", nil)
-	req.Header.Set("X-Forwarded-Host", "evil.com:8080")
-	rec := httptest.NewRecorder()
-	s.handler.ServeHTTP(rec, req)
-	s.Equal(http.StatusNotFound, rec.Code)
-}
-
-func (s *AllowedHostsTestSuite) TestForwardedHostValid() {
-	req := httptest.NewRequest("GET", "https://company-proxy.com:8080/v1/config", nil)
-	req.Header.Set("Forwarded", "host=76.125.27.44:8080")
-	rec := httptest.NewRecorder()
-	s.handler.ServeHTTP(rec, req)
-	s.Equal(http.StatusOK, rec.Code)
-}
-
-func (s *AllowedHostsTestSuite) TestForwardedHostInvalid() {
-	req := httptest.NewRequest("GET", "https://company-proxy.com:8080/v1/config", nil)
-	req.Header.Set("Forwarded", "host=77.125.26.44:8080")
-	rec := httptest.NewRecorder()
-	s.handler.ServeHTTP(rec, req)
-	s.Equal(http.StatusNotFound, rec.Code)
+	for _, scenario := range scenarios {
+		req := httptest.NewRequest("GET", "https://company-proxy.com:8080/v1/config", nil)
+		req.Header.Set(scenario.inputHeaderKey, scenario.inputHeaderVal)
+		rec := httptest.NewRecorder()
+		s.handler.ServeHTTP(rec, req)
+		s.Equal(scenario.expectedStatus, rec.Code)
+	}
 }
 
 func (s *AllowedHostsTestSuite) TestDefaultPortNoTLSValid() {
