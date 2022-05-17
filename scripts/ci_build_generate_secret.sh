eca6882fa97a53e4a5cdf6c3c67ed92154ac9238
Muhammad Noman <Muhammadnoman@folio3.com>
2022-05-17 20:04:35 +0500

    tesst

--- scripts/ci_build_generate_secret.sh
+++ scripts/ci_build_generate_secret.sh
@@ -10,7 +10,7 @@ for os in linux darwin windows; do
     -e GOARCH=amd64 \
     -v $PWD:/workdir \
     -v /tmp/output_packages:/output \
-    -it golang:${GIMME_GO_VERSION%.x} \
+    -i golang:${GIMME_GO_VERSION%.x} \
     bash -c "cd /workdir && make clean && make build_generate_secret \
     && cp /workdir/bin/generate_secret /output/generate_secret${windows:+.exe} \
     && tar cvfz /output/generate_secret-$os-amd64-$APP_VERSION.tar.gz -C /output generate_secret${windows:+.exe} \
