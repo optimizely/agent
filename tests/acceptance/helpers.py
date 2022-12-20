f00a7169b491bbc83fac42db9aedccf82aa650ed
Yasir Ali <yali@folio3.com>
2022-12-20 12:08:29 +0500

    fixes.

--- tests/acceptance/helpers.py
+++ tests/acceptance/helpers.py
@@ -4,7 +4,7 @@ import string
 from random import randint, choice
 
 import yaml
-from openapi_core import OpenAPISpec as Spec
+from openapi_core import Spec
 from openapi_core.validation.request.datatypes import (OpenAPIRequest, RequestParameters)
 from openapi_core.validation.request.validators import RequestValidator
 from openapi_core.validation.response.datatypes import OpenAPIResponse
