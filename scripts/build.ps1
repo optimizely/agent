49dedd50f630c3552fb474efb128431c2c9df1f6
Yasir Ali <yali@folio3.com>
2023-02-27 17:06:51 -0500

    fixes.

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -81,9 +81,10 @@ function refreshPath {
 
 function installDeps {
     Write-Host "Installing build dependencies for Optimizely Agent..." -ForegroundColor Green
-    go env -w GO111MODULE=off
-	go get -u github.com/rakyll/statik
+    $env:GO111MODULE = "off"
+    go get -u github.com/rakyll/statik
     statik -src=api/openapi-spec
+    $env:GO111MODULE = "on"
 }
 
 function runOptimizelyAgentTests {
