5c31242ee1e17b18fe8be5000025ad1786790d59
Yasir Folio3 <39988750+yasirfolio3@users.noreply.github.com>
2023-05-19 13:08:19 -0400

    1. Adds tests for odp datafile changes. (#393)
    2. Removes unnecessary documentation sync files.

---
+++ tests/acceptance/odp_datafile.py
@@ -0,0 +1,246 @@
+odp_datafile = {
+    "groups": [
+
+    ],
+    "environmentKey": "production",
+    "rollouts": [
+        {
+            "experiments": [
+                {
+                    "status": "Running",
+                    "audienceConditions": [
+
+                    ],
+                    "audienceIds": [
+
+                    ],
+                    "variations": [
+                        {
+                            "variables": [
+
+                            ],
+                            "id": "151743",
+                            "key": "off",
+                            "featureEnabled": False
+                        }
+                    ],
+                    "forcedVariations": {
+
+                    },
+                    "key": "default-rollout-52207-23726430538",
+                    "layerId": "rollout-52207-23726430538",
+                    "trafficAllocation": [
+                        {
+                            "entityId": "151743",
+                            "endOfRange": 10000
+                        }
+                    ],
+                    "id": "default-rollout-52207-23726430538"
+                }
+            ],
+            "id": "rollout-52207-23726430538"
+        },
+        {
+            "experiments": [
+                {
+                    "status": "Running",
+                    "audienceConditions": [
+
+                    ],
+                    "audienceIds": [
+
+                    ],
+                    "variations": [
+                        {
+                            "variables": [
+
+                            ],
+                            "id": "151797",
+                            "key": "off",
+                            "featureEnabled": False
+                        }
+                    ],
+                    "forcedVariations": {
+
+                    },
+                    "key": "default-rollout-52231-23726430538",
+                    "layerId": "rollout-52231-23726430538",
+                    "trafficAllocation": [
+                        {
+                            "entityId": "151797",
+                            "endOfRange": 10000
+                        }
+                    ],
+                    "id": "default-rollout-52231-23726430538"
+                }
+            ],
+            "id": "rollout-52231-23726430538"
+        }
+    ],
+    "typedAudiences": [
+        {
+            "id": "23783030150",
+            "conditions": [
+                "and",
+                [
+                  "or",
+                  [
+                      "or",
+                      {
+                          "value": "mac",
+                          "type": "custom_attribute",
+                          "name": "laptop_os",
+                          "match": "exact"
+                      }
+                  ],
+                    [
+                      "or",
+                      {
+                          "value": "atsbugbashsegmentdob",
+                          "type": "third_party_dimension",
+                          "name": "odp.audiences",
+                          "match": "qualified"
+                      }
+                  ],
+                    [
+                      "or",
+                      {
+                          "value": "atsbugbashsegmentgender",
+                          "type": "third_party_dimension",
+                          "name": "odp.audiences",
+                          "match": "qualified"
+                      }
+                  ],
+                    [
+                      "or",
+                      {
+                          "value": "atsbugbashsegmenthaspurchased",
+                          "type": "third_party_dimension",
+                          "name": "odp.audiences",
+                          "match": "qualified"
+                      }
+                  ]
+                ]
+            ],
+            "name": "Customer Information"
+        }
+    ],
+    "projectId": "23743870473",
+    "variables": [
+
+    ],
+    "featureFlags": [
+        {
+            "experimentIds": [
+                "9300000235908"
+            ],
+            "rolloutId": "rollout-52207-23726430538",
+            "variables": [
+
+            ],
+            "id": "52207",
+            "key": "flag1"
+        },
+        {
+            "experimentIds": [
+
+            ],
+            "rolloutId": "rollout-52231-23726430538",
+            "variables": [
+
+            ],
+            "id": "52231",
+            "key": "flag2"
+        }
+    ],
+    "integrations": [
+        {
+            "publicKey": "ax6UV2223fD-jpOXID0BMg",
+            "host": "https://api.zaius.com",
+            "key": "odp"
+        }
+    ],
+    "experiments": [
+        {
+            "status": "Running",
+            "audienceConditions": [
+                "or",
+                "23783030150"
+            ],
+            "audienceIds": [
+                "23783030150"
+            ],
+            "variations": [
+                {
+                    "variables": [
+
+                    ],
+                    "id": "151799",
+                    "key": "variation_a",
+                    "featureEnabled": True
+                },
+                {
+                    "variables": [
+
+                    ],
+                    "id": "151800",
+                    "key": "variation_b",
+                    "featureEnabled": True
+                }
+            ],
+            "forcedVariations": {
+
+            },
+            "key": "ab_experiment",
+            "layerId": "9300000198165",
+            "trafficAllocation": [
+                {
+                    "entityId": "151799",
+                    "endOfRange": 5000
+                },
+                {
+                    "entityId": "151800",
+                    "endOfRange": 10000
+                }
+            ],
+            "id": "9300000235908"
+        }
+    ],
+    "version": "4",
+    "audiences": [
+        {
+            "id": "23783030150",
+            "conditions": "[\"or\", {\"match\": \"exact\", \"name\": \"$opt_dummy_attribute\", \"type\": \"custom_attribute\", \"value\": \"$opt_dummy_value\"}]",
+            "name": "Customer Information"
+        },
+        {
+            "conditions": "[\"or\", {\"match\": \"exact\", \"name\": \"$opt_dummy_attribute\", \"type\": \"custom_attribute\", \"value\": \"$opt_dummy_value\"}]",
+            "id": "$opt_dummy_audience",
+            "name": "Optimizely-Generated Audience for Backwards Compatibility"
+        }
+    ],
+    "anonymizeIP": True,
+    "sdkKey": "91GuiKYH8ZF1hLLXR7DR1",
+    "attributes": [
+        {
+            "id": "23749050365",
+            "key": "laptop_os_version"
+        },
+        {
+            "id": "23767440563",
+            "key": "laptop_os"
+        }
+    ],
+    "botFiltering": False,
+    "accountId": "10845721364",
+    "events": [
+        {
+            "experimentIds": [
+                "9300000235908"
+            ],
+            "id": "23746420417",
+            "key": "myevent"
+        }
+    ],
+    "revision": "18"
+}
--- tests/acceptance/test_acceptance/test_datafile.py
+++ tests/acceptance/test_acceptance/test_datafile.py
@@ -1,5 +1,7 @@
 from tests.acceptance.helpers import ENDPOINT_DATAFILE, create_and_validate_request_and_response
 from tests.acceptance.datafile import datafile as expected_response
+from tests.acceptance.odp_datafile import odp_datafile as expected_odp_response
+
 
 
 def test_datafile_success(session_obj):
@@ -17,4 +19,21 @@ def test_datafile_success(session_obj):
                                                     payload=payload, params=params)
 
     assert expected_response == resp.json()
+    assert resp.status_code == 200, resp.text
+
+def test_datafile_success_odp(session_override_sdk_key_odp):
+    """
+    Normally a good practice is to have expected response as a string like in other tests.
+    Here we are exceptionally making expected response a dict for easier comparison.
+    String was causing some issues with extra white space characters.
+    :param session_obj: session object
+    """
+    payload = '{"userId": "matjaz", "userAttributes": {"attr_1": "hola"}}'
+    params = {"featureKey": "feature_1"}
+
+    resp = create_and_validate_request_and_response(ENDPOINT_DATAFILE, 'get', session_override_sdk_key_odp,
+                                                    bypass_validation_request=False,
+                                                    payload=payload, params=params)
+
+    assert expected_odp_response == resp.json()
     assert resp.status_code == 200, resp.text
