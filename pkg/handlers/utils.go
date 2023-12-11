c8cf2eb916a47d0ae5ff06afa1f2576eec1daba2
Pulak Bhowmick <pulak.bhowmick@optimizely.com>
2023-12-11 21:43:07 +0600

    add traceid for error log

--- pkg/handlers/utils.go
+++ pkg/handlers/utils.go
@@ -24,6 +24,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/render"
+	"go.opentelemetry.io/otel/trace"
 
 	"github.com/optimizely/agent/pkg/middleware"
 )
@@ -35,7 +36,8 @@ type ErrorResponse struct {
 
 // RenderError sets the request status and renders the error message.
 func RenderError(err error, status int, w http.ResponseWriter, r *http.Request) {
-	middleware.GetLogger(r).Info().Err(err).Int("status", status).Msg("render error")
+	span := trace.SpanFromContext(r.Context())
+	middleware.GetLogger(r).Err(err).Int("status", status).Str("traceID", span.SpanContext().TraceID().String()).Str("spanID", span.SpanContext().SpanID().String()).Msg("render error")
 	render.Status(r, status)
 	render.JSON(w, r, ErrorResponse{Error: err.Error()})
 }
@@ -44,22 +46,25 @@ func RenderError(err error, status int, w http.ResponseWriter, r *http.Request) 
 // into the provided interface. Note that we're sanitizing the returned error
 // so that it is not leaked back to the requestor.
 func ParseRequestBody(r *http.Request, v interface{}) error {
+	logger := middleware.GetLogger(r)
+	span := trace.SpanFromContext(r.Context())
+
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		msg := "error reading request body"
-		middleware.GetLogger(r).Error().Err(err).Msg(msg)
+		logger.Err(err).Str("traceID", span.SpanContext().TraceID().String()).Str("spanID", span.SpanContext().SpanID().String()).Msg(msg)
 		return fmt.Errorf("%s", msg)
 	}
 
 	if len(body) == 0 {
-		middleware.GetLogger(r).Debug().Msg("body was empty skip JSON unmarshal")
+		logger.Info().Str("traceID", span.SpanContext().TraceID().String()).Str("spanID", span.SpanContext().SpanID().String()).Msg("body was empty skip JSON unmarshal")
 		return nil
 	}
 
 	err = json.Unmarshal(body, &v)
 	if err != nil {
 		msg := "error parsing request body"
-		middleware.GetLogger(r).Error().Err(err).Msg(msg)
+		logger.Err(err).Str("traceID", span.SpanContext().TraceID().String()).Str("spanID", span.SpanContext().SpanID().String()).Msg(msg)
 		return fmt.Errorf("%s", msg)
 	}
 
