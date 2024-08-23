58c0d4fdc2a1a9af3297f5435b1b3fa58b4319d9
pulak-opti <pulak.bhowmick@episerver.com>
2024-08-23 13:41:47 +0600

    update tests

--- tests/acceptance/test_acceptance/test_activate.py
+++ tests/acceptance/test_acceptance/test_activate.py
@@ -496,7 +496,7 @@ expected_activate_with_config = """[
   {
     "enabled": true,
     "experimentKey": "ab_test1",
-    "featureKey": "flag_ab_test1",
+    "featureKey": "GkbzTurBWXr8EtNGZj2j6e",
     "type": "feature",
     "userId": "matjaz",
     "variationKey": "variation_1"
