f87ed420bb77020c0bfc3d3cd2a6e645703cbe44
pulak-opti <pulak.bhowmick@episerver.com>
2024-10-08 01:00:51 +0600

    fix test

--- tests/acceptance/test_acceptance/conftest.py
+++ tests/acceptance/test_acceptance/conftest.py
@@ -43,7 +43,7 @@ def session_override_sdk_key(session_obj):
     :param session_obj: session fixture object
     :return: updated session object
     """
-    session_obj.headers['X-Optimizely-SDK-Key'] = 'xxx_invalid_sdk_key_xxx'
+    session_obj.headers['X-Optimizely-SDK-Key'] = 'invalidsdkkey'
     return session_obj
 
 
